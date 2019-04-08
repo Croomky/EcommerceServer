a97533e68d04716560f875c3fbc68fa3ad97ceb3
unknown <konrad96pawlak@gmail.com>
2019-04-08 19:49:48 +0200

    add args to get function

--- product/mixins.py
+++ product/mixins.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 
 class JsonResponseMixin:
-    def get(self, request, id):
+    def get(self, request, *args, **kwargs):
         serializer = self.serializer_class(self.get_queryset(), many=True)
         return JsonResponse(serializer.data, safe=False)
