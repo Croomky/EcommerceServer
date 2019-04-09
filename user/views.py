7cd671fe9afc8a4513f66b13980d86672b102bcf
unknown <konrad96pawlak@gmail.com>
2019-04-09 16:00:53 +0200

    add primitive login endpoint

--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.http import JsonResponse
+from rest_framework.views import APIView
 
-# Create your views here.
+class LoginView(APIView):
+    def post(self, request):
+        user = authenticate(
+            username=request.data.get('username'),
+            password=request.data.get('password')
+        )
+
+        if user is not None:
+            return JsonResponse({
+                'answer': 'ok'
+            })
+        else:
+            return JsonResponse({
+                'answer': 'no'
+            })
